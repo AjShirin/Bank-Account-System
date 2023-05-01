3e333f1668f741cf1e315d2419ce75d03254293b
AjShirin <shirinajmimc98@gmail.com>
2023-05-01 10:09:44 +0400

    first commit

---
+++ src/main/java/com/BankSystem/SecondProject_BankSystem/SecondProjectBankSystemApplication.java
@@ -0,0 +1,13 @@
+package com.BankSystem.SecondProject_BankSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjectBankSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProjectBankSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BankSystem/SecondProject_BankSystem/SecondProjectBankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BankSystem.SecondProject_BankSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondProjectBankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
